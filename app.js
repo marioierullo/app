32855528c5971b30461d7ed0819dcd6acdf27937
Glitch (wild-cumbersome-gymnast) <none>
2024-03-04 04:05:49 +0000

    🚠🏮 Checkpoint ./app.js:65608976/2710

--- app.js
+++ app.js
@@ -89,20 +89,20 @@ app.post('/interactions', async function (req, res) {
   }
   
   if (type === InteractionType.MESSAGE_COMPONENT) {
-// custom_id set in payload when sending message component
-const componentId = data.custom_id;
+    // custom_id set in payload when sending message component
+    const componentId = data.custom_id;
 
-  if (componentId.startsWith('accept_button_')) {
-    // get the associated game ID
-    const gameId = componentId.replace('accept_button_', '');
-    // Delete message with token in request body
-    const endpoint = `webhooks/${process.env.APP_ID}/${req.body.token}/messages/${req.body.message.id}`;
-    try {
-      await res.send({
-        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-        data: {
+    if (componentId.startsWith('accept_button_')) {
+      // get the associated game ID
+      const gameId = componentId.replace('accept_button_', '');
+      // Delete message with token in request body
+      const endpoint = `webhooks/${process.env.APP_ID}/${req.body.token}/messages/${req.body.message.id}`;
+      try {
+        await res.send({
+          type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+          data: {
             // Fetches a random emoji to send from a helper function
-            content: 'What is your object of choice?',
+            content: '¿Que objeto vas a seleccionar?',
             // Indicates it'll be an ephemeral message
             flags: InteractionResponseFlags.EPHEMERAL,
             components: [
@@ -126,6 +126,44 @@ const componentId = data.custom_id;
         console.error('Error sending message:', err);
       }
     }
+    else if (componentId.startsWith('select_choice_')) {
+      // get the associated game ID
+      const gameId = componentId.replace('select_choice_', '');
+
+      if (activeGames[gameId]) {
+        // Get user ID and object choice for responding user
+        const userId = req.body.member.user.id;
+        const objectName = data.values[0];
+        // Calculate result from helper function
+        const resultStr = getResult(activeGames[gameId], {
+          id: userId,
+          objectName,
+        });
+
+        // Remove game from storage
+        delete activeGames[gameId];
+        // Update message with token in request body
+        const endpoint = `webhooks/${process.env.APP_ID}/${req.body.token}/messages/${req.body.message.id}`;
+
+        try {
+          // Send results
+          await res.send({
+            type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+            data: { content: resultStr },
+          });
+          // Update ephemeral message
+          await DiscordRequest(endpoint, {
+            method: 'PATCH',
+            body: {
+            content: 'Nice choice ' + getRandomEmoji(),
+            components: []
+            }
+          });
+        } catch (err) {
+          console.error('Error sending message:', err);
+        }
+      }
+    }    
   }
 });
 
