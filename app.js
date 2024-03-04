a086eb47564d29e22708611a923a5c51a7922312
Glitch (wild-cumbersome-gymnast) <none>
2024-03-04 03:29:50 +0000

    💅📮 Checkpoint ./app.js:65608976/2205

--- app.js
+++ app.js
@@ -52,6 +52,40 @@ app.post('/interactions', async function (req, res) {
         },
       });
     }
+    
+    // "challenge" command
+    if (name === 'challenge' && id) {
+        const userId = req.body.member.user.id;
+        // User's object choice
+        const objectName = req.body.data.options[0].value;
+
+        // Create active game using message ID as the game ID
+        activeGames[id] = {
+            id: userId,
+            objectName,
+        };
+
+        return res.send({
+        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+        data: {
+            content: `Desafio a un duelo a Los Tres Grandes de <@${userId}>`,
+            components: [
+            {
+                type: MessageComponentTypes.ACTION_ROW,
+                components: [
+                {
+                    type: MessageComponentTypes.BUTTON,
+                    // Append the game ID to use later on
+                    custom_id: `accept_button_${req.body.id}`,
+                    label: 'Accept',
+                    style: ButtonStyleTypes.PRIMARY,
+                },
+                ],
+            },
+            ],
+        },
+        });
+    }
   }
 });
 
